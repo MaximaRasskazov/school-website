c559e6b05d2ae5172bf5222f5497e368356a20ce
Maxima <MaximaRasskazov@gmail.com>
2025-11-20 23:18:36 +0500

    fix: canvas:

--- scripts/game.js
+++ scripts/game.js
@@ -9,7 +9,7 @@ const gameOverMessage = document.getElementById('gameOverMessage');
 
 // Настройки размера холста
 const CANVAS_WIDTH = 300;
-const CANVAS_HEIGHT = 600;
+const CANVAS_HEIGHT = 800;
 
 // Установка размера холста
 canvas.width = CANVAS_WIDTH;
